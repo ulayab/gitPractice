bb72c531b5edb174ac7d2bb2bfc6e749c8c0c48e
ulayab <gary0626@gmail.com>
2018-05-28 09:22:16 +0800

    a practice of Fibonacci

---
+++ Fib.js
@@ -0,0 +1,33 @@
+//Fibonacci
+
+var readline = require('readline');
+
+
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Please input the number(1 to 10): ', function (num){
+      
+    
+  
+        console.log('For the num ' +num + ', your Fib num is ' + Fibonacci());
+        rl.close();
+        process.stdin.destroy();
+    
+ }); 
+    
+
+function Fibonacci(num){
+    if(num===1 || num===2){
+        return 1;
+    }else if(num>2 || num<=10 ){
+       return(Fibonacci(num-1) + Fibonacci(num-2));
+    }else{
+        console.log('Please input the num between 1 to 10.')
+    }
+}
+//進行了遞歸運算，記得添加判斷條件，否則遞歸會無限循環下去(Maximum call stack size exceeded)
+
