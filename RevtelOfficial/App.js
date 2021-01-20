53ecaa434d3534598eb97f44f4f8214324e8c64f
Ula Chao <gary0626@gmail.com>
2021-01-20 13:03:49 +0800

    feat: modify icons in tabs navigator

--- RevtelOfficial/App.js
+++ RevtelOfficial/App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/Screens/HomeScreen';
@@ -32,7 +33,28 @@ const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'bullhorn';
+            } else if (route.name === 'Favorite') {
+              iconName = 'heart';
+            } else if (route.name === 'About') {
+              iconName = 'keyboard-o';
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Favorite" component={FavoriteScreen} />
         <Tab.Screen name="About" component={AboutScreen} />
